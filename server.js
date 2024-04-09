0556541acd1a3d5d2ef1a189434b4afb9a8949c1
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-04-09 16:02:32 -0300

    teste

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const jsonServer = require('json-server');
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-const middleware = jsonServer.defaults();
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
+server.use(middlewares);
+// Add this before server.use(router)
 server.use(
-    jsonServer.rewriter({
-        "/*":"/$1"
-    })
-)
-
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
 server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
-server.listen(3000,()=>{
-    console.log("Json server esta rodando")
-})
-
+// Export the Server API
 module.exports = server;
